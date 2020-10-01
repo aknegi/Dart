0d08059b78289874edc2d55758f91bb80897d833
Akshay Negi <43299819+aknegi@users.noreply.github.com>
2020-10-01 14:57:02 +0530

    Update Leader.dart

--- Assignments/first Assignment/Leader.dart
+++ Assignments/first Assignment/Leader.dart
@@ -1,12 +1,16 @@
 void main(List<String> args) {
   
+  //giver List
   var arr=[4,15,3,7,0];
   
+  //declaring an empty list which will store the leaders
   var leaders=[];
   int i=arr.length-1;
   int j=0;
   int max=0;
-  while(i>=0)
+  
+ 
+  while(i>=0)  // iterating from last element to first element
   {
     if(arr[i]>=max)
     {
@@ -17,6 +21,7 @@ void main(List<String> args) {
     i--;
   }
 
+  // printing the leaders
   for(int k=j-1;k>=0;k--)
   {
     print(leaders[k]);
