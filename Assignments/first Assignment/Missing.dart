c01e208366c0339a1ea3f800ef821b0f59c201aa
Akshay Negi <43299819+aknegi@users.noreply.github.com>
2020-10-01 14:59:08 +0530

    Update Missing.dart

--- Assignments/first Assignment/Missing.dart
+++ Assignments/first Assignment/Missing.dart
@@ -2,19 +2,21 @@
 
 void main() {
   
+  //taking an array Named input
 var input=[1,2,3,4,5,6,8,9];
-int len=input.length+1;
+
 
 int sum=0;
 int arSum=0;
 int z=0;
 for( z=0;z<input.length;z++)
 {
-  arSum= arSum + input[z];
-  sum=sum+(z+1);
+  arSum= arSum + input[z]; //sum of elements of array
+  sum=sum+(z+1); //sum of 1 to N-1 elements
 }
-sum=sum+(z+1);
-print(sum-arSum);
+  
+sum=sum+(z+1); // sum of Nthe element
+print(sum-arSum); //printing the missing elements
 
 
 }
