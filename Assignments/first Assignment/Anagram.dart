6d8a86abe7aa6f4c59033cd1f6565a20f71c4f91
Akshay Negi <43299819+aknegi@users.noreply.github.com>
2020-10-01 14:46:53 +0530

    Update Anagram.dart

--- Assignments/first Assignment/Anagram.dart
+++ Assignments/first Assignment/Anagram.dart
@@ -1,17 +1,25 @@
 void main() {
   
-var check="hello";
-var meck="lleoh";
+var check="hello";  //First String
+var meck="lleoh";  //Second String
+  
+  //first checking lengths of both strings are equal or not
+  // if lenghts are not equal then Strings will not be anagram
  if(check.length==meck.length)
  {
+   
+  //convertin Strings to array using split() method
   var first=check.split("");
   var second=meck.split("");
 
+   //Sorting String array
   first.sort();
   second.sort();
   var flag=0;
 
-
+// Comparing both array elements
+   //if all are equal to each other, then Strings are anagram
+   //else Strings are not anagram
   for(int i=0;i<first.length;i++)
   {
     if(first[i]!=second[i])
